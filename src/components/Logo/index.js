335ad109bdf8be6e83eb52dc5c4b294e03717e42
Elyas Rodrigues <elyasrz11@gmail.com>
2022-12-03 13:49:27 -0300

    finish project

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom"
 export function Logo(){
   return(
     <Link to="/">
-      <h1 className="logo">Dev<span className="logoText">Link</span></h1>
+      <h1 className="logo">R<span className="logoText">Link's</span></h1>
     </Link>
   )
 }
